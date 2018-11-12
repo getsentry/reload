9e29140a7d9a0e116fd52c1c2c3a1a9ef0ddcad1
Billy Vong <billy@sentry.io>
2018-11-12 10:52:18 -0800

    rename + add metrics

--- reload_app/metrics/__init__.py
+++ reload_app/metrics/__init__.py
@@ -1,9 +1,26 @@
 VALID_METRICS = {
-    'component.render': [
-        # component name
-        'name'
-    ],
-    'initial_load': [],
+    ### metrics that occur inside of the React application ###
+    # TODO:
+    #  'app.component.render': [
+    #     # component name
+    #     'name'
+    #  ],
+    #  'app.component.api': [],
+    #  'app.component.dynamic-import-fail': [],
+
+    ### metrics related to page/script loads ###
+
+    # time it takes to load scripts in <head>
+    'app.page.head-load': [],
+
+    # time it takes to load everything at the end of <body>
+    'app.page.body-load': [],
+
+    # time it takes to fetch, parse, and execute main js app
+    'app.page.bundle-load': [],
+
+    # when the main js bundle fails to load
+    'app.page.bundle-load-fail': [],
 }
 
 VALID_METRIC_TYPES = set(['increment', 'gauge'])
