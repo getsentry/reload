71ce533b115dec08fd489763bd7fedc7dd599e36
Alberto Leal <mail4alberto@gmail.com>
2019-06-20 15:45:40 -0400

    feat(redirect): Add event to track deprecated URLs (#115)
    Part of SEN-772

--- reload_app/events.py
+++ reload_app/events.py
@@ -237,6 +237,12 @@ VALID_EVENTS = {
         "org_id": int,
         "project_id": int,
     },
+    # Track the redirects from deprecated URLs to newer URLs
+    "deprecated_urls.redirect": {
+        "feature": str,
+        "url": str,  # the URL being redirected from
+        "org_id": int
+    },
     "member_limit_modal.seen": {
         "org_id": int,
     },
