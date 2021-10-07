2c73e6848b9ac647aa3afe94c96f3eb4b5fdc037
Stephen Cefali <scefali@sentry.io>
2021-10-07 11:16:22 -0700

    merge from master

--- reload_app/app.py
+++ reload_app/app.py
@@ -147,7 +147,11 @@ class App(Router):
         except Exception:
             return Response("bad request expecting json\n", status=400)
 
-        if data.get("event_name") not in VALID_EVENTS:
+
+        # pop off allow_no_schema since we don't want to pass it
+        allow_no_schema = data.pop("allow_no_schema", False)
+        event_has_schema = data.get("event_name") in VALID_EVENTS
+        if not event_has_schema and not allow_no_schema:
             return Response("bad request check if valid event name\n", status=400)
 
         clean_data = {
@@ -174,20 +178,25 @@ class App(Router):
                 clean_data[field] = data[field]
             except KeyError:
                 pass
+        
+        # validate schema if it exists
+        if event_has_schema:
+            for field, type_expected in VALID_EVENTS[data["event_name"]].items():
+                if field not in data or data[field] is None:
+                    continue
+                try:
+                    type_expected(data[field])
+                except ValueError:
+                    sentry_sdk.capture_exception()
+                    return Response("bad request maybe check field type\n", status=400)
 
-        for field, type_expected in VALID_EVENTS[data["event_name"]].items():
-            if field not in data or data[field] is None:
-                continue
-            try:
-                type_expected(data[field])
-            except ValueError:
-                sentry_sdk.capture_exception()
-                return Response("bad request maybe check field type\n", status=400)
-
-            type_received = type(data[field])
-            if type_expected != type_received:
-                logger.error("expected %s, received %s for field %s of event %s" % (type_expected, type_received, field, data["event_name"]))
-            clean_data[field] = data[field]
+                type_received = type(data[field])
+                if type_expected != type_received:
+                    logger.error("expected %s, received %s for field %s of event %s" % (type_expected, type_received, field, data["event_name"]))
+                clean_data[field] = data[field]
+        else:
+            # blindly pass fields otherwise
+            clean_data.update(data)
 
         # Conforms to super-big-data.analytics.events schema.
         row = {
